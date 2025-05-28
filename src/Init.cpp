e0e490aa536322ed8be59b440188f979f2b57a3a
Wenbin <1501110084@pku.edu.cn>
2025-05-27 22:00:31 -0700

    Use newest tables

--- src/Init.cpp
+++ src/Init.cpp
@@ -563,25 +563,42 @@ void Init::readInNucleusConfigs(
             fileName = "C12_VMC.bin.in";
         } else if (lightNucleusOption == 3) {
             fileName = "C12_alphaCluster.bin.in";
+        } else if (lightNucleusOption == 4) {
+            fileName = "coords_12C_NCSM_aligned.bin";
+        } else if (lightNucleusOption == 5) {
+            fileName = "coords_12C_NCSM_mixed.bin";
         }
     } else if (nucleusA == 16) {
-        if (lightNucleusOption == 2) {
-            fileName = "O16_VMC.bin.in";
-        } else if (lightNucleusOption == 3) {
+        fileName = "O16_VMC.bin.in";
+        if (lightNucleusOption == 1) {
             fileName = "O16_alphaCluster.bin.in";
+        } else if (lightNucleusOption == 2) {
+            fileName = "O16_PGCM_clustered_dmin0.bin.in";
+        } else if (lightNucleusOption == 3) {
+            fileName = "O16_PGCM_uniform_dmin0.bin.in";
         } else if (lightNucleusOption == 4) {
-            fileName = "O16_PGCM.bin.in";
+            fileName = "O16_NLEFT_dmin0.5fm_positiveweights.bin.in";
         } else if (lightNucleusOption == 5) {
-            fileName = "O16_NLEFT.bin.in";
+            fileName = "O16_NLEFT_dmin0.5fm_negativeweights.bin.in";
         }
     } else if (nucleusA == 20) {
-        fileName = "Ne20_PGCM.bin.in";
+        fileName = "Ne20_PGCM_clustered_dmin0.bin.in";
+        if (lightNucleusOption == 3) {
+            fileName = "Ne20_PGCM_uniform_dmin0.bin.in";
+        } else if (lightNucleusOption == 4) {
+            fileName = "Ne20_NLEFT_dmin0.5fm_positiveweights.bin.in";
+        } else if (lightNucleusOption == 5) {
+            fileName = "Ne20_NLEFT_dmin0.5fm_negativeweights.bin.in";
+        }
     } else if (nucleusA == 40) {
         fileName = "Ar40_VMC.bin.in";
+    } else if (nucleusA == 197) {
+        fileName = "Au197.bin.in";
+    } else if (nucleusA == 208) {
+        fileName = "Pb208.bin.in";
     } else {
         readFlag = false;
     }
-
     if (!readFlag) return;
 
     fileName = path + fileName;
